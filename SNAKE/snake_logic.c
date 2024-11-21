bde315793772147288ba9674e3d97ec1803b0a31
GodlikeVarijor <xklimam00@vutbr.cz>
2024-11-21 21:08:28 +0100

    update snake_logic.c

--- SNAKE/snake_logic.c
+++ SNAKE/snake_logic.c
@@ -9,3 +9,41 @@ static vector2 screen = {MAX_WIDTH, MAX_HEIGHT};
 static vector2 dir;
 static int score;
 static bool running;
+
+void initialize_game() {
+    head.x = screen.x / 2;
+    head.y = screen.y / 2;
+    food.x = rand() % screen.x;
+    food.y = rand() % screen.y;
+    dir.x = 1;
+    dir.y = 0;
+    score = 0;
+    running = true;
+}
+
+bool collide(vector2 a, vector2 b) {
+    return a.x == b.x && a.y == b.y;
+}
+
+void update() {
+    vector2 next = {head.x + dir.x, head.y + dir.y};
+
+    if (collide(next, food)) {
+        score++;
+        head = next;
+        food.x = rand() % screen.x;
+        food.y = rand() % screen.y;
+    } else {
+        for (int i = score; i > 0; i--) {
+            if (collide(next, head)) {
+                running = false;
+                break;
+            }
+        }
+        head = next;
+    }
+
+    if (head.x < 0 || head.x >= screen.x || head.y < 0 || head.y >= screen.y) {
+        running = false;
+    }
+}
