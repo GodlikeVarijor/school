f137b2e8f0d290e829d1165569598d5c5f90a494
GodlikeVarijor <xklimam00@vutbr.cz>
2024-12-01 20:30:38 +0100

    update obstacles.c

--- SNAKE_GAME/obstacles.c
+++ SNAKE_GAME/obstacles.c
@@ -6,8 +6,13 @@ void load_obstacles(const char* filename, Obstacles* obstacles) {
     FILE* file = fopen(filename, "r");
     if (file) {
         obstacles->count = 0;
-        while (fscanf(file, "%d %d", &obstacles->obstacles[obstacles->count].x, &obstacles->obstacles[obstacles->count].y) != EOF) {
-            obstacles->count++;
+        int x, y;
+        while (fscanf(file, "%d %d", &x, &y) != EOF) {
+            if (x > 0 && x < WIDTH - 1 && y > 0 && y < HEIGHT - 1) {
+                obstacles->obstacles[obstacles->count].x = x;
+                obstacles->obstacles[obstacles->count].y = y;
+                obstacles->count++;
+            }
         }
         fclose(file);
     }
